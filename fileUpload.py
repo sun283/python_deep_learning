0654dcc4ea565abeb0da41e3ce62ba17c3067200
sun283 <asalia283@gmail.com>
2022-04-24 23:38:20 +0900

    google drive test day02

--- fileUpload.py
+++ fileUpload.py
@@ -18,12 +18,41 @@ drive = GoogleDrive(gauth)
 # fileList = drive.ListFile({'q': "'<folder ID>' in parents and trashed=false"}).GetList()
 # print(fileList)
 
+def get_children(root_folder_id):
+    str = "\'" + root_folder_id + "\'" + " in parents and trashed=false"
+    file_list = drive.ListFile({'q': str}).GetList()
+    return file_list
+
+def get_folder_id(root_folder_id, root_folder_title):
+    file_list = get_children(root_folder_id)
+    for file in file_list:
+        if(file['title'] == root_folder_title):
+            return file['id']
+def add_children_to_tree(tree, file_list, parent_id):
+    for file in file_list:
+        tree.create_node(file['title'], file['id'], parent=parent_id)
+        # For debugging
+        # print('parent: %s, title: %s, id: %s' % (parent_id, file['title'], file['id']))
+### Go down the tree until you reach a leaf ###
+def populate_tree_recursively(tree,parent_id):
+    children = get_children(parent_id)
+    add_children_to_tree(tree, children, parent_id)
+    if(len(children) > 0):
+        for child in children:
+            populate_tree_recursively(tree, child['id'])
+
 ## Constant
-# FOLDER_ID = 'google drive folder id'
-FOLDER_ID = '1IDZRVrOrW2UrhzsMSq3jJXfhaEL0F42m'
+ROOT_FOLDER_TITLE = "my-top-level-root-folder-name"
+ROOT_FOLDER_ID = get_folder_id("root", ROOT_FOLDER_TITLE)
+FOLDER_ID = 'Google Drive Folder Id'
 FILES = (
     ('file.csv'),
 )
+
+tree = Tree()
+tree.create_node(ROOT_FOLDER_TITLE, ROOT_FOLDER_ID)
+populate_tree_recursively(tree, ROOT_FOLDER_ID)
+tree.show()
 
 ## Upload file in Google Drive
 # file = drive.CreateFile({'title': 'file.csv', "parents": [{"id": FOLDER_ID}]})

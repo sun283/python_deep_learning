681bf85903924b4f5b7478bac5d270884e2c1c69
sun283 <asalia283@gmail.com>
2022-04-27 22:05:57 +0900

    google drive test day 05

--- fileUpload.py
+++ fileUpload.py
@@ -39,21 +39,21 @@ def generateData(month):
     output_file = OUTPUT_PREFIX + month_string + OUTPUT_EXTENSION
     print('INPUT : ' + input_file, '  OUTPUT : ' + output_file)
     data = pd.read_csv(input_file, sep=",", encoding="euc-kr")
-    # Clean : 결측치, 오류치를 제외
-    data_clean = data.drop(['Unnamed: 6'], axis='columns')
-    data_clean = data_clean[data_clean.통행시간 > 0]
+    # 결측치, 오류치를 제외
+    data = data.drop(['Unnamed: 6'], axis='columns')
+    data = data[data.통행시간 > 0]
     # Select : 필요한 컬럼만 선택
     # 101: 서울, 105: 기흥, 110: 목천, 115: 대전, 120: 황간, 125: 남구미, 130: 동김천, 135: 경주, 140: 부산
-    df_data = pd.DataFrame(data_clean, columns=["집계일자", "집계시", "출발영업소코드", "도착영업소코드", "통행시간"])
-    start_from_101 = df_data[df_data.출발영업소코드 == 101]
-    start_from_101_to_140 = start_from_101[start_from_101['도착영업소코드'].isin([105,110,115,120,125,130,135,140])]
+    data = pd.DataFrame(data, columns=["집계일자", "집계시", "출발영업소코드", "도착영업소코드", "통행시간"])
+    data = data[data.출발영업소코드 == 101]
+    data = data[data['도착영업소코드'].isin([105,110,115,120,125,130,135,140])]
     # Convert : 집계일자를 요일로 변환하여 삽입
-    start_from_101_to_140 = start_from_101_to_140.assign(요일=pd.to_datetime(start_from_101_to_140['집계일자'], format='%Y%m%d').dt.dayofweek)
+    data = data.assign(요일=pd.to_datetime(data['집계일자'], format='%Y%m%d').dt.dayofweek)
     # Sort
-    start_from_101_to_140.sort_values(by=['집계일자', '집계시'])
+    data.sort_values(by=['집계일자', '집계시'])
     # Save per month
-    start_from_101_to_140.to_csv(output_file, index=None, header=True, encoding='euc-kr')
-    output_dataframes.append(start_from_101_to_140)
+    data.to_csv(output_file, index=None, header=True, encoding='euc-kr')
+    output_dataframes.append(data)
 
 # Save into Google Drive
 def uploadData():

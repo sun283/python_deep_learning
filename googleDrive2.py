90684029485ad7ccc67bdef93216818a5fba137a
sun283 <asalia283@gmail.com>
2022-05-01 22:39:14 +0900

    google drive test day09

--- googleDrive2.py
+++ googleDrive2.py
@@ -11,65 +11,27 @@ gauth.LocalWebserverAuth()
 # drive = GoogleDrive(gauth) create a Google Drive object to handle file. You will be using this object to list and create file.
 drive = GoogleDrive(gauth)
 
-## Connecting to Google Drive
 # will get you the list of files/folders in your Google Drive. 
 # It will also give you the detail of those files/folders. 
 # We capture the folder ID of the folder you would like to upload files to. 
 # In this case, file['title'] needs to be match with the folder I would upload the files to.
 # View all folders and file in your Google Drive
-      
-# 1) Choose your starting point by inserting file name
-folder_title = "your-starting-point-folder"
-fileID = ''
-
-# 2) Retrieve the folder id - start searching from root
-## Accessing files in folders
-# You can use the ListFile to get the files but this time change the root to file ID.
-file_list = drive.ListFile({'q': "'root' in parents and trashed=false"}).GetList()
-for file in file_list:
-    print('Title: %s, ID: %s' % (file['title'], file['id']))
-    # Get the folder ID that you want
-    if(file['title'] == folder_title):
-        fileID = file['id']
-        break
-
-# 3) Build string dynamically (need to use escape characters to support single quote syntax)
-str = "\'" + fileID + "\'" + " in parents and trashed=false"    
-
-# 4) Starting iterating over files
-file_list = drive.ListFile({'q': str}).GetList()
-for file in file_list:
-    print('title: %s, id: %s' % (file['title'], file['id']))
-
-## Listing and uploading file in Google Drive
-# drive.CreateFile() accepts metadata(dict.) as input to initialize a GoogleDriveFile. 
-# I initialized a file with "mimeType" : "text/csv" and "id" : fileID. 
-# This id will specific where the file will be uploaded to. 
-# In this case, the file will be uploaded to the folder To Share.
-file1 = drive.CreateFile({"mimeType": "text/csv", "parents": [{"kind": "drive#fileLink", "id": fileID}]})
-file1.SetContentFile("data/data_2021.csv")
-# Upload the file.
-file1.Upload()
-print('Created file %s with mimeType %s' % (file1['title'], file1['mimeType']))   
-
-# Initialize GoogleDriveFile instance with file id.
-file2 = drive.CreateFile({'id': fileID})
-file2.Trash()  # Move file to trash.
-file2.UnTrash()  # Move file out of trash.
-file2.Delete()  # Permanently delete the file.
-# file_list = drive.ListFile({'q': "'<folder ID>' in parents and trashed=false"}).GetList()
-# we can get into folder picture inside the folder To Share.
-# create a GoogleDriveFile with the specified file ID. Use Trash() to move file to trash. You can also use Delete() to delete the file permanently.
 
 ### Some basic helper functions ### 
 def get_children(root_folder_id):
+    # 3) Build string dynamically (need to use escape characters to support single quote syntax)
     str = "\'" + root_folder_id + "\'" + " in parents and trashed=false"
+    # 4) Starting iterating over files
     file_list = drive.ListFile({'q': str}).GetList()
     return file_list
 
+# 2) Retrieve the folder id - start searching from root
+# Accessing files in folders
+# You can use the ListFile to get the files but this time change the root to file ID.
 def get_folder_id(root_folder_id, root_folder_title):
     file_list = get_children(root_folder_id)
     for file in file_list:
+        print('Title: %s, ID: %s' % (file['title'], file['id']))
         if(file['title'] == root_folder_title):
             return file['id']
 
@@ -87,8 +49,32 @@ def populate_tree_recursively(tree,parent_id):
         for child in children:
             populate_tree_recursively(tree, child['id'])
 
+## Listing and uploading file in Google Drive
+# drive.CreateFile() accepts metadata(dict.) as input to initialize a GoogleDriveFile. 
+# I initialized a file with "mimeType" : "text/csv" and "id" : fileID. 
+# This id will specific where the file will be uploaded to. 
+# In this case, the file will be uploaded to the folder To Share.
+def uploadFile(fileID):
+    file1 = drive.CreateFile({"mimeType": "text/csv", "parents": [{"kind": "drive#fileLink", "id": fileID}]})
+    file1.SetContentFile("data/data_2021.csv")
+    # Upload the file.
+    file1.Upload()
+    print('Created file %s with mimeType %s' % (file1['title'], file1['mimeType']))   
+    # Initialize GoogleDriveFile instance with file id.
+    file2 = drive.CreateFile({'id': fileID})
+    # Move file to trash
+    # create a GoogleDriveFile with the specified file ID.
+    # Use Trash() to move file to trash.
+    # You can also use Delete() to delete the file permanently.
+    file2.Trash()
+    # Move file out of trash
+    file2.UnTrash()
+    # Permanently delete the file
+    file2.Delete()
+
 ### Create the tree and the top level node ###
 def main():
+    # 1) Choose your starting point by inserting file name
     root_folder_title = "my-top-level-root-folder-name"
     root_folder_id = get_folder_id("root", root_folder_title)
 

3fe1102f7b87b3e43023f72cd40c05252eeeacc3
sun283 <asalia283@gmail.com>
2022-05-06 12:22:16 +0900

    codingTestExample04

--- 100001stPrime.py
+++ 100001stPrime.py
@@ -2,31 +2,13 @@
 # By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 # What is the 10,001st prime number?
 
-import numpy as np
+def primes(n):
+    primes = [2]
+    attempt = 3
+    while len(primes) < n:
+        if all(attempt % prime != 0 for prime in primes):
+            primes.append(attempt)
+        attempt += 2
+    return primes[-1]
 
-MAX_SIZE = 1000000
-def SOE(primelst):
-    is_prime = np.ones(MAX_SIZE)
-    
-    prime = 2
-    while (prime * prime) < MAX_SIZE:
-        if is_prime[prime] == True:
-            i = (prime * prime)
-            while i < MAX_SIZE:
-                is_prime[i] = 0
-                i += prime
-        prime += 1
-
-    prime = 2
-    while prime < MAX_SIZE:
-        if is_prime[prime]:
-            primelst.append(prime)
-
-        prime += 1
-
-    return primelst
-
-result = SOE([])
-
-n = 10001
-print(result[n - 1])
+print(primes(10001))
--- summationOfPrimes.py
+++ summationOfPrimes.py
@@ -3,3 +3,46 @@
 # The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 # Find the sum of all the primes below two million.
 
+# def is_prime(number):
+#     factors = [candidate_factor for candidate_factor in range(1, number+1) if number % candidate_factor == 0]
+#     return len(factors) == 2
+
+# number = 10
+
+# #generates a list of numbers.
+# primes = [number for number in range(2, 101) if is_prime(number)]
+# prime_total = sum(primes)
+# print (prime_total)
+
+def sumOfPrimes(n):
+    # list to store prime numbers
+    prime = [True] * (n + 1)
+
+    # Create a boolean array "prime[0..n]"
+    # and initialize all entries it as true.
+    # A value in prime[i] will finally be
+    # false if i is Not a prime, else true.
+     
+    p = 2
+    while p * p <= n:
+        # If prime[p] is not changed, then
+        # it is a prime
+        if prime[p] == True:
+            # Update all multiples of p
+            i = p * 2
+            while i <= n:
+                prime[i] = False
+                i += p
+                print(i)
+        p += 1   
+          
+    # Return sum of primes generated through
+    # Sieve.
+    sum = 0
+    for i in range (2, n + 1):
+        if(prime[i]):
+            sum += i
+    return sum
+ 
+n = 2000000
+print(sumOfPrimes(n))

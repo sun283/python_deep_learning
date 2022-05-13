1af1ccb1044c53d17453cd8f939d9f8ec1816d00
sun283 <asalia283@gmail.com>
2022-05-13 23:16:01 +0900

    codingTestExample11

--- timeApiLog.py
+++ timeApiLog.py
@@ -13,16 +13,6 @@ import logging
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s :: %(name)s :: %(levelname)s :: %(message)s')
 logger = logging.getLogger(__name__)
 
-# def word_count(myfile):
-#     try:
-#         with open(myfile, 'r') as f:
-#             file_data = f.read()
-#             words = file_data.split(" ")
-#             final_word_count = len(words)
-#             logger.info("this file has %d words", final_word_count)
-#             return final_word_count
-#     except OSError as e:
-#         logger.error("error reading the file")
 def hypotenuse(a, b):
     """Compute the hypotenuse"""
     return (a**2 + b**2)**0.5

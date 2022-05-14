5da354e6cf550d377f0cdc59125273abd0cf5b32
sun283 <asalia283@gmail.com>
2022-05-14 21:22:26 +0900

    codingTestExample12

--- timeApiLog3.py
+++ timeApiLog3.py
@@ -1,36 +1,47 @@
-# refs https://github.com/Delgan/loguru
-from loguru import logger
+# Refs https://www.loggly.com/ultimate-guide/python-logging-basics/
+#Logging to Standard Output for Systemd
 
-logger.debug("That's it, beautiful and simple logging!")
-logger.add(sys.stderr, format="{time} {level} {message}", filter="my_module", level="INFO")
-logger.add("file_{time}.log")
-logger.add("file_1.log", rotation="500 MB")    # Automatically rotate too big file
-logger.add("file_2.log", rotation="12:00")     # New file is created each day at noon
-logger.add("file_3.log", rotation="1 week")    # Once the file is too old, it's rotated
-
-logger.add("file_X.log", retention="10 days")  # Cleanup after some time
-
-logger.add("file_Y.log", compression="zip")    # Save some loved space
-logger.info("If you're using Python {}, prefer {feature} of course!", 3.6, feature="f-strings")
-
-@logger.catch
-def my_function(x, y, z):
-    # An error? It's caught anyway!
-    return 1 / (x + y + z)
-
-logger.add(sys.stdout, colorize=True, format="<green>{time}</green> <level>{message}</level>")
-logger.add("somefile.log", enqueue=True)
-
-logger.add("out.log", backtrace=True, diagnose=True)  # Caution, may leak sensitive data in prod
-
-def func(a, b):
-    return a / b
-
-def nested(c):
-    try:
-        func(5, c)
-    except ZeroDivisionError:
-        logger.exception("What?!")
-
-nested(0)
-
+import logging
+import os
+ 
+class OneLineExceptionFormatter(logging.Formatter):
+    def formatException(self, exc_info):
+        result = super().formatException(exc_info)
+        return repr(result)
+ 
+    def format(self, record):
+        result = super().format(record)
+        if record.exc_text:
+            result = result.replace("\n", "")
+        return result
+ 
+handler = logging.StreamHandler()
+formatter = OneLineExceptionFormatter(logging.BASIC_FORMAT)
+handler.setFormatter(formatter)
+root = logging.getLogger()
+root.setLevel(os.environ.get("LOGLEVEL", "INFO"))
+root.addHandler(handler)
+ 
+try:
+    exit(main())
+except Exception:
+    logging.exception("Exception in main(): ")
+    exit(1)
+    
+# import logging
+# import logging.handlers
+# import os
+ 
+# handler = logging.handlers.WatchedFileHandler(
+#     os.environ.get("LOGFILE", "/var/log/yourapp.log"))
+# formatter = logging.Formatter(logging.BASIC_FORMAT)
+# handler.setFormatter(formatter)
+# root = logging.getLogger()
+# root.setLevel(os.environ.get("LOGLEVEL", "INFO"))
+# root.addHandler(handler)
+ 
+# try:
+#     exit(main())
+# except Exception:
+#     logging.exception("Exception in main()")
+#     exit(1)

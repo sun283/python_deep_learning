f85d3fa516f50e02e470b1904a3f5b84d63dd665
sun283 <asalia283@gmail.com>
2022-05-22 14:27:27 +0900

    codingTestExample16

--- findLowestPositiveInteger.py
+++ findLowestPositiveInteger.py
@@ -6,11 +6,19 @@
 # The input [1, 2, 0] should give 3.
 # You can modify the input array in-place.
 
-array = [3, 4, -1, 1]
-# array = [1, 2, 0]
+# array = [3, 4, -1, 1]
+array = [1, 2, 0]
 
 def findLowestPositiveInteger(array):
     num = 0
+    array.sort()
+    for i in range(array[0], len(array)):
+        if i > 0 and i not in array:
+            num = num + i
+            return num
+        elif i > 0 and i == array[-1]:
+            num = i + 1
+            return num
+
+print(findLowestPositiveInteger(array))
     
-    
-    return num

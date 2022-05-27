78df65a936d5c9c0d86708e421437d8da18145b5
sun283 <asalia283@gmail.com>
2022-05-27 21:19:52 +0900

    python data structure 5

--- findLargestSum.py
+++ findLargestSum.py
@@ -3,3 +3,35 @@
 # For example, [2, 4, 6, 2, 5] should return 13, since we pick 2, 6, and 5.
 # [5, 1, 1, 5] should return 10, since we pick 5 and 5.
 # Follow-up: Can you do this in O(N) time and constant space?
+
+import sys
+ 
+# Function to calculate the maximum sum in a given list
+# with no adjacent elements considered
+# `i` ——> index of the current element
+# `prev` ——> index of the previous element included in the sum
+def findMaxSumSubsequence(nums, i, n, prev=-sys.maxsize):
+ 
+    # base case: all elements are processed
+    if i == n:
+        return 0
+ 
+    # recur by excluding the current element
+    excl = findMaxSumSubsequence(nums, i + 1, n, prev)
+ 
+    incl = 0
+ 
+    # include current element only if it's not adjacent to
+    # the previous element
+    if prev + 1 != i:
+        incl = findMaxSumSubsequence(nums, i + 1, n, i) + nums[i]
+ 
+    # return maximum sum we get by including or excluding
+    # current item
+    return max(incl, excl)
+ 
+ 
+if __name__ == '__main__':
+ 
+    nums = [1, 2, 9, 4, 5, 0, 4, 11, 6]
+    print('The maximum sum is', findMaxSumSubsequence(nums, 0, len(nums)))
